efa3fcb4b2d18e7ef3d8f458b7751e209c6ea335
hamida-mstafa <hamidamstafa@gmail.com>
2018-11-28 18:36:56 +0300

    save changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask_login import login_required
 
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route(', methods = ['GET','POST'])
 @login_required
 def new_review(id):
